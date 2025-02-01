601ad1c8159c4979a43b659c3dbef090fa8f8f8f
alexdelargy <addelargy97@gmail.com>
2025-02-01 17:59:03 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,148 @@
+from enum import Enum
+import time 
+import pandas as pd 
+import numpy as np
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.impute import KNNImputer, SimpleImputer
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder, MinMaxScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.decomposition import PCA
+from sklearn.metrics import root_mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeClassifier
+
+class Steps(Enum):
+    INTRO = 1
+    READFILE = 2
+    DISPLAYFILES = 3
+    SETPROBLEM = 4
+    CLEANDATA = 5
+    INITMODEL = 6
+
+class CustomModel:
+    
+    def __init__(self, df, modelType, x_features, y_feature):
+        self.df = df
+        self.modelType = modelType
+        self.x_features = x_features
+        self.y_feature = y_feature
+
+    def generatePreprocessor(self, scalerType=StandardScaler):
+        numerical_pipeline = Pipeline([('Imputer', KNNImputer(n_neighbors=5)),
+                               ('Scaler', scalerType())])
+
+        categorical_pipeline = Pipeline([('Encoder', OrdinalEncoder()),
+                                 ('Imputer', KNNImputer(n_neighbors=5))])
+
+        data_pipeline = ColumnTransformer([('numerical', numerical_pipeline, self.df.select_dtypes(exclude='object').columns),
+                                   ('categorical', categorical_pipeline, self.generateModeldf.select_dtypes(include='object').columns)])
+
+        return data_pipeline
+    
+    def generateModel(self, preprocessor, modelType='linear'):
+        if modelType == 'Linear':
+            model = LinearRegression()
+            param_grid = {'model__fit_intercept': [True, False]}
+
+        elif modelType == 'Logistic':
+            model = LogisticRegression()
+            c_space = np.logspace(-5, 8, 15)
+            param_grid = {'C': c_space, 'penalty': ['l1', 'l2']}
+
+        elif modelType == 'RandomForest':
+            model = RandomForestRegressor()
+            param_grid = {'n_estimators': [50, 100, 200, 300],
+                          'max_features': ['auto', 'sqrt', 'log2'],
+                          'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
+                          'min_samples_split': [2, 5, 10],
+                          'min_samples_leaf': [1, 2, 4],
+                          'bootstrap': [True, False]}
+            
+        elif modelType == 'SVM':
+            model = SVR()
+            param_grid = {'C': [0.1, 1, 10, 100, 1000],
+                          'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
+                          'kernel': ['rbf']}
+
+        elif model == 'DecisionTree':
+            model = DecisionTreeClassifier()
+            param_grid = {'criterion': ['gini', 'entropy'],
+                          'splitter': ['best', 'random'],
+                          'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
+                          'min_samples_split': [2, 5, 10],
+                          'min_samples_leaf': [1, 2, 4]}
+            
+        return GridSearchCV(model, param_grid, cv=5)
+    
+    def trainModel(self):
+        X = self.df[self.x_features]
+        y = self.df[self.y_feature]
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        
+        preprocessor = self.generatePreprocessor()
+        X_train_preprocessed = preprocessor.fit_transform(X_train)
+        model = self.generateModel(preprocessor, modelType=self.modelType)
+        
+        model.fit(X_train, y_train)
+        print(f"Best Parameters: {model.best_params_}")
+        
+        return model, X_test, y_test
+        
+        
+    
+def run():
+    opModeIsActive = True
+    currentStep = Steps.INTRO
+    
+    while opModeIsActive:
+        match currentStep.name:
+            case "INTRO":
+                print("Welcome to blackbox AI, are you ready?")
+                time.sleep(3)
+                currentStep = Steps.READFILE
+                
+            case "READFILE":
+                f = input("Input filename: ")
+                try:
+                    df = pd.read_csv(f)
+                    currentStep = Steps.DISPLAYFILES
+                except FileNotFoundError:
+                    print("That file was not found. Try again.")
+                    currentStep = Steps.READFILE
+                    
+            case "DISPLAYFILES":
+                print(f"\nFile: {f} loaded.")
+                print("Here's a summary of your file: \n")
+                print(df.info())
+                print("\n\n")
+                print(df.head(5))
+                currentStep = Steps.SETPROBLEM
+    
+            case "SETPROBLEM":
+                predictionType = input("Select ML Type: ")
+                print(f"Available Features: {list(df.columns)}")
+                features = input("Select features: ").split(', ')
+                print(f"\nAvailable Response Vars: {set(df.columns)-set(features)}")
+                response = input("Select response: ")
+                currentStep = Steps.INITMODEL
+                
+            case "INITMODEL":
+                model = CustomModel(df, predictionType, features, response)
+                trainedModel, X_test, y_test = model.trainModel()
+                y_pred = trainedModel.predict(X_test)
+                print(f"RMSE: {root_mean_squared_error(y_test, y_pred)}")
+                currentStep = Steps.INTRO
+                opModeIsActive = False
+                
+if __name__ == "__main__":
+    run()
+    
+    
+    
+    
